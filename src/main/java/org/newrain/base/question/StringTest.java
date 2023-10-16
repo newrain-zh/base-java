6369a0aa953deacd58f6b5c4059e6b8d980b7cd4
alex <zzqnobody@126.com>
2023-10-16 14:58:21 +0800

    字符串切割示例

--- src/main/java/org/newrain/base/question/StringTest.java
+++ src/main/java/org/newrain/base/question/StringTest.java
@@ -1,17 +1,19 @@
 package org.newrain.base.question;
 
+import java.util.Arrays;
+
 /**
- * Created by zzq on 16-10-20.
+ * 字符串切割示例
  */
 public class StringTest {
 
 
     public static void main(String[] args) {
         String str = "12.03";
-        String[] res = str.split(".");    //出错！！！
-        for (int i = 0; i < res.length; i++)
-            System.out.println(res[i]);
-        System.out.println(res.toString());
+        String[] res = str.split("."); //res数组为空
+        //String[] res = str.split("\\."); //正确使用方式
+        for (String re : res) System.out.println(re);
+        System.out.println(Arrays.toString(res));
     }
 
 }
