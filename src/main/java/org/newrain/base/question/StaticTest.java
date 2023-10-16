009582e5d9bdd32386f353b2a9884c5c35600052
alex <zzqnobody@126.com>
2023-10-16 15:01:01 +0800

    静态代码块执行顺序示例

--- src/main/java/org/newrain/base/question/StaticTest.java
+++ src/main/java/org/newrain/base/question/StaticTest.java
@@ -1,26 +1,27 @@
 package org.newrain.base.question;
 
 /**
- * Created by zzqno on 2017-5-8.
+ * static 静态代码块示例
+ * 调整静态块顺序 执行顺序也有不同
  */
 public class StaticTest {
 
     static int cnt = 6;
+
     static {
         cnt /= 3;
         System.out.println("static2: " + cnt);
     }
-
 
     static {
         cnt += 9;
         System.out.println("static1: " + cnt);
     }
 
+
     public static void main(String[] args) {
         System.out.println("cnt =" + cnt);
     }
-
 
 
 }
